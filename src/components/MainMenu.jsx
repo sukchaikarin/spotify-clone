47ca570815820dabc1cf2004773cd75b8a8efed9
bluebirds2920 <110462756+bluebirds2920@users.noreply.github.com>
2024-02-23 16:07:27 +0700

    set-text-color(MainMenu.jsx)

--- src/components/MainMenu.jsx
+++ src/components/MainMenu.jsx
@@ -1,11 +1,7 @@
 import React from "react";
 
 const MainMenu = () => {
-  return (
-    <div className="flex bg-amber-300 text-lime-500 h-1/4 text-black">
-      MainMenu
-    </div>
-  );
+  return <div className="flex bg-amber-300 h-1/4 text-black">MainMenu</div>;
 };
 
 export default MainMenu;
